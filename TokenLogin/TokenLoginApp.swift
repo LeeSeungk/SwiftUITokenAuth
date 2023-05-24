cf2228fb67552f82ca37a97f7368d4d47ea30728
이승규 <dltmdrb3084@naver.com>
2023-05-24 10:07:59 +0900

    Initial Commit

---
+++ TokenLogin/TokenLoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  TokenLoginApp.swift
+//  TokenLogin
+//
+//  Created by SeungKyu on 2023/05/24.
+//
+
+import SwiftUI
+
+@main
+struct TokenLoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
